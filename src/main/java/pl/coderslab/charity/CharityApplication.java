3c4ec5361c351b2f449f2eb76dcd0cd9d6c08784
Olga <olga.kornieva1607@gmail.com>
2023-06-19 23:00:59 +0200

    first commit

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -9,5 +9,6 @@ public class CharityApplication {
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
     }
-
 }
+
+
--- src/test/java/pl/coderslab/charity/CharityApplicationTests.java
+++ src/test/java/pl/coderslab/charity/CharityApplicationTests.java
@@ -9,5 +9,4 @@ class CharityApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
