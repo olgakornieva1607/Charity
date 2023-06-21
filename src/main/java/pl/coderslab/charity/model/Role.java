26130a55f3f1f1279e3c82529859560e17b53e73
Olga <olga.kornieva1607@gmail.com>
2023-06-21 21:10:31 +0200

    added entity Role

---
+++ src/main/java/pl/coderslab/charity/model/Role.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+}
