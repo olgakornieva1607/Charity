87478e649ee1c7d41d9b24b2a1fdb50a15a37ddf
Olga <olga.kornieva1607@gmail.com>
2023-06-22 14:21:35 +0200

    added AuthController

---
+++ src/main/java/pl/coderslab/charity/controller/AuthController.java
@@ -0,0 +1,21 @@
+package pl.coderslab.charity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationForm(){
+        return "register";
+    }
+}
